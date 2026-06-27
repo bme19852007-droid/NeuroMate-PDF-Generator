a8f06d6e9ecddcc7312019c1bf50a17992ff9f25
bme19852007-droid <bme19852007@gmail.com>
2026-06-27 13:19:26 +0300

    Update utils.py

--- neuromate/utils.py
+++ neuromate/utils.py
@@ -1,1 +1,8 @@
-neuromate/utils.py
+import arabic_reshaper
+from bidi.algorithm import get_display
+
+
+def fix_text(text: str) -> str:
+    if not text:
+        return ""
+    return get_display(arabic_reshaper.reshape(text))
